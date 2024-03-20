2ef753167fb299921f2986cc5f90d11980ae5688
Larissa Martins <larissa.martins@inboundcycle.com>
2024-03-20 20:44:10 -0300

    Feat: add automatic login, validate token and handling errors

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
-import { TOKEN_POST, USER_GET } from './api';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
 
 export const UserContext = createContext();
 
@@ -11,13 +11,34 @@ export const UserStorage = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  // validar token antes de realizar login automático
+  useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token');
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error('Token inválido');
+          await getUser(token);
+        } catch (err) {
+          userLogout(); // resetar dados em caso de erro
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, []);
+
   async function getUser(token) {
     const { url, options } = USER_GET(token);
     const response = await fetch(url, options);
     const json = await response.json();
     setData(json);
     setLogin(true);
-    console.log(json);
   }
 
   // logar usuario
@@ -29,8 +50,18 @@ export const UserStorage = ({ children }) => {
     getUser(token);
   }
 
+  // logout usuario
+  async function userLogout() {
+    // resetar tudo ao valor inicial
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem('token');
+  }
+
   return (
-    <UserContext.Provider value={{ userLogin, data }}>
+    <UserContext.Provider value={{ userLogin, userLogout, data }}>
       {children}
     </UserContext.Provider>
   );
