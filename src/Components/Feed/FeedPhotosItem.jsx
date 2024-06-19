65a39f5dd09ec37380d20f1bb4c4f18614346339
Larissa Martins <larissa.martins@inboundcycle.com>
2024-06-19 16:50:17 -0300

    Feat: loading img skeleton

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './FeedPhotosItem.module.css';
+import Image from '../Helper/Image';
 
 const FeedPhotosItem = ({ photo, setModalPhoto }) => {
   console.log('PHOTO.SCR' + photo.src); // Log the image source
   function handleClick() {
-    setModalPhoto(photo); 
+    setModalPhoto(photo);
   }
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.view}>{photo.acessos}</span>
     </li>
   );
